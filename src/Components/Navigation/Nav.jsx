8b17edd32da398976986cd6fc0158ef5a314f81d
Prakritig01 <prakritigupta1112002@gmail.com>
2024-11-22 20:13:09 +0530

    css added to nav bar

--- src/Components/Navigation/Nav.jsx
+++ src/Components/Navigation/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Nav.css';
 import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <div>
       <nav className='tab'>
-        <NavLink to= "/">Expense Form</NavLink>
-        <NavLink to= "/expenses">Expense List</NavLink>
+        <NavLink to= "/" className= "tabLink">Expense Form</NavLink>
+        <NavLink to= "/expenses" className= "tabLink">Expense List</NavLink>
       </nav>
     </div>
   )
